75d9ea9166c6f02529440c78ed85be9f1d5484d6
Your Name <you@example.com>
2022-05-04 04:28:47 +0300

    put  commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-require ('dotenv').config();
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use (cors());
-app.use (express.json());
+app.use(cors());
+app.use(express.json());
 
 
 
@@ -17,70 +17,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-      await client.connect();
-      const collection = client.db("I-House").collection("Items");
-      
-      app.post('/item', async(req, res) =>{
-          const newItem = req.body;
-          console.log(newItem);
-          const result = await collection.insertOne(newItem);
-          res.send(result);
-      });
+        await client.connect();
+        const collection = client.db("I-House").collection("Items");
 
-      app.get('/item', async(req, res) =>{
-          const query = {};
-          const cursor = collection.find(query);
-          const items = await cursor.toArray();
-          res.send(items);
-      })
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            console.log(newItem);
+            const result = await collection.insertOne(newItem);
+            res.send(result);
+        });
 
-      app.get('/item/:id', async(req,res) =>{
-          const id = req.params.id;
-          const query = {_id:ObjectId(id)};
-          console.log(query);
-          const singleItem = await collection.findOne(query);
-          res.send(singleItem);
+        app.get('/item', async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        })
 
-      })
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const singleItem = await collection.findOne(query);
+            res.send(singleItem);
 
-      app.put('/item/:id', async(req, res)=>{
-        const id = req.params.id;
-        const updatedUser = req.body;
-        console.log(updatedUser)
-         const query = { _id:ObjectId(id) };
-        const options = { upsert: true };
-        const updateDoc = {
-            $set: {
-             quantity : updatedUser.quantity,
-             
-            } 
-          };
-        const result = await collection.updateOne(query, updateDoc, options );
-        res.send(result);
-       
+        })
 
-    })
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedService = req.body;
+            console.log(updatedService)
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    productName: updatedService.productName,
+                    price: updatedService.price,
+                    quantity: updatedService.quantity,
+                    image: updatedService.image,
+                    supplier: updatedService.supplier,
+                    description: updatedService.description,
+                }
+            };
+            const result = await collection.updateOne(query, updateDoc, options);
+            res.send(result);
 
-      app.delete('/item/:id', async(req, res) =>{
-          const id = req.params.id;
-          const query = {_id:ObjectId(id)};
-          console.log(query);
-          const result = await collection.deleteOne(query);
-          res.send(result);
-      })
-    
+
+        })
+
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(query);
+            const result = await collection.deleteOne(query);
+            res.send(result);
+        })
+
 
 
     } finally {
-      /* await client.close(); */
+        /* await client.close(); */
     }
-  }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send ('running I-House Backend')
+    res.send('running I-House Backend')
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('I-House server running');
 });
